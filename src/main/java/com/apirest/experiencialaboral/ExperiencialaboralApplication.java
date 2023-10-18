82082c007c0764d65703730ed6ee018bb6668b0d
Jarood <jaroodjimili@gmail.com>
2023-10-17 21:40:06 -0500

    iniciando proyecto api rest

---
+++ src/main/java/com/apirest/experiencialaboral/ExperiencialaboralApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.experiencialaboral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperiencialaboralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperiencialaboralApplication.class, args);
+	}
+
+}
